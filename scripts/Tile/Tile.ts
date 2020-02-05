dbdb2e1673f43cdf3956f1b62c0e820dff71e20b
SvenFrankson <sven.frankson@gmail.com>
2020-02-05 14:02:40 +0100

    Generating Tile Mesh 1 / n.

--- scripts/Tile/Tile.ts
+++ scripts/Tile/Tile.ts
@@ -1,4 +1,6 @@
-var TILE_SIZE = 8;
+var TILE_SIZE = 9;
+var DX = 0.7;
+var DY = 0.3;
 
 interface TileData {
     i: number;
@@ -29,14 +31,25 @@ class Tile extends BABYLON.Mesh {
         }
     }
 
+    private static _Signs = [[- 1, - 1], [1, - 1], [1, 1], [- 1, 1]];
     public updateTerrainMesh(): void {
         let data = new BABYLON.VertexData();
         let positions: number[] = [];
         let colors: number[] = [];
         let indices: number[] = [];
 
-        for (let i = 0; i < TILE_SIZE; i++) {
-            
+        for (let j = 0; j < TILE_SIZE; j++) {
+            for (let i = 0; i < TILE_SIZE; i++) {
+                let y = this.heights[i][j] * DY * 3;
+
+                Tile._Signs.forEach(signs => {
+                    let x = 2 * i * DX + signs[0] * DX * 0.5;
+                    let z = 2 * j * DX + signs[1] * DX * 0.5;
+                    x = Math.max(0, x);
+                    z = Math.max(0, z);
+                    positions.push(x, y, z);
+                });
+            }
         }
 
         data.positions = positions;
